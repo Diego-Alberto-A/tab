47ab1318e42276f3491a515b1452e84c1e6e28fc
Diego-Alberto-A <a01376064@tec.mx>
2026-05-05 23:05:24 -0600

    Parte 2

--- lib/tab.ex
+++ lib/tab.ex
@@ -1,8 +1,27 @@
 defmodule Tablature do
   def parse(tab) do
-    Regex.scan(~r/\d+/, tab)
-    |> List.flatten
-    |> Enum.map(fn n -> "B" <> n end)
+    tab
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [string, notes, _] = String.split(line, "|")
+
+      String.graphemes(notes)
+      |> Enum.map(fn c ->
+        if c =~ ~r/\d/ do
+          "#{string}#{c}"
+        else
+          ""
+        end
+      end)
+    end)
+    |> Enum.zip()
+    |> Enum.map(fn tuple ->
+      tuple
+      |> Tuple.to_list()
+      |> Enum.join(" ")
+    end)
+    |> Enum.join(" ")
+    |> String.split()
     |> Enum.join(" ")
   end
 end
