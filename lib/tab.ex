70163126c064e55721b1769b00bb7eeede2da767
Diego-Alberto-A <a01376064@tec.mx>
2026-05-05 22:05:07 -0600

    Parte 1

--- lib/tab.ex
+++ lib/tab.ex
@@ -1,18 +1,8 @@
-defmodule Tab do
-  @moduledoc """
-  Documentation for `Tab`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tab.hello()
-      :world
-
-  """
-  def hello do
-    :world
+defmodule Tablature do
+  def parse(tab) do
+    Regex.scan(~r/\d+/, tab)
+    |> List.flatten
+    |> Enum.map(fn n -> "B" <> n end)
+    |> Enum.join(" ")
   end
 end
