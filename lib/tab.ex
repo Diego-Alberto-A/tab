aa77e7862035398845a14f4b5f17349930305ce6
Diego-Alberto-A <a01376064@tec.mx>
2026-05-06 00:01:19 -0600

    Parte 5

--- lib/tab.ex
+++ lib/tab.ex
@@ -1,64 +1,76 @@
 defmodule Tablature do
   def parse(tab) do
+    # separamos segmentos
     tab
-    # separamos el texto en lineas
-    |> String.split("\n", trim: true)
+    |> String.split("\n\n", trim: true)
 
-    |> Enum.map(fn line ->
-      # dividimos cada linea
-      [string | parts] = String.split(line, "|")
+    |> Enum.map(fn segment ->
+      segment
+      # separamos el texto en lineas
+      |> String.split("\n", trim: true)
 
-      # quitamos compases vacios
-      parts = Enum.reject(parts, fn x -> x == "" end)
+      |> Enum.map(fn line ->
+        # dividimos cada linea
+        [string | parts] = String.split(line, "|")
 
-      # recorremos cada compas
-      Enum.map(parts, fn notes ->
+        # quitamos compases vacios
+        parts = Enum.reject(parts, fn x -> x == "" end)
 
-        # separamos las notas
-        String.graphemes(notes)
+        # recorremos cada compas
+        Enum.map(parts, fn notes ->
 
-        |> Enum.map(fn c ->
-          # checamos si es de 0-9
-          if c =~ ~r/\d/ do
-            # juntamos cuerda + nota
-            "#{string}#{c}"
+          # separamos las notas
+          String.graphemes(notes)
+
+          |> Enum.map(fn c ->
+            # checamos si es de 0-9
+            if c =~ ~r/\d/ do
+              # juntamos cuerda + nota
+              "#{string}#{c}"
+            else
+              # si no es numero, dejamos vacio
+              ""
+            end
+          end)
+        end)
+      end)
+
+      # acomodamos los compases
+      |> Enum.zip()
+
+      |> Enum.flat_map(fn measure ->
+        measure
+        |> Tuple.to_list()
+
+        # acomodamos las notas por columnas
+        |> Enum.zip()
+
+        |> Enum.map(fn tuple ->
+          tuple
+          |> Tuple.to_list()
+
+          # unimos notas simultaneas con /
+          |> Enum.filter(fn x -> x != "" end)
+          |> Enum.join("/")
+        end)
+
+        # detectamos silencios largos
+        |> Enum.chunk_by(fn x -> x == "" end)
+
+        |> Enum.flat_map(fn group ->
+          if hd(group) == "" and length(group) >= 3 do
+            if length(group) >= 5 do
+              ["_", "_", "_"]
+            else
+              ["_"]
+            end
           else
-            # si no es numero, dejamos vacio
-            ""
+            Enum.reject(group, fn x -> x == "" end)
           end
         end)
       end)
-    end)
 
-    # acomodamos los compases
-    |> Enum.zip()
-
-    |> Enum.flat_map(fn measure ->
-      measure
-      |> Tuple.to_list()
-
-      # acomodamos las notas por columnas
-      |> Enum.zip()
-
-      |> Enum.map(fn tuple ->
-        tuple
-        |> Tuple.to_list()
-
-        # unimos notas simultaneas con /
-        |> Enum.filter(fn x -> x != "" end)
-        |> Enum.join("/")
-      end)
-
-      # detectamos silencios largos
-      |> Enum.chunk_by(fn x -> x == "" end)
-
-      |> Enum.flat_map(fn group ->
-        if hd(group) == "" and length(group) >= 3 do
-          ["_"]
-        else
-          Enum.reject(group, fn x -> x == "" end)
-        end
-      end)
+      |> Enum.join(" ")
     end)
 
     |> Enum.join(" ")
